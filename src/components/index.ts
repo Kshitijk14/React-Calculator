9433234d419487dec249e19fda9aa823cb7849d0
Kshitij Chaturvedi <kshitijhrms@gmail.com>
2023-07-28 17:59:10 +0530

    calculator web-app

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Wrapper from "./Wrapper";
+import Screen from "./Screen";
+import Button from "./Button";
+import ButtonBox from "./ButtonBox";
+
+export { Wrapper, Screen, Button, ButtonBox }
